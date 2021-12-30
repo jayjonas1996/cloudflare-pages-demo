b6b338bd0bf06ed8b0a2570238013a051a7c28df
jayjonas1996 <jayjonas1996@gmail.com>
2021-12-30 00:43:48 -0500

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    posts: [],
+  };
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    fetch('https://falling-frost-9a99.jaynaik.workers.dev/posts', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    }).then((res) => console.log(res.json()));
+
+    return (
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+    );
+  }
 }
